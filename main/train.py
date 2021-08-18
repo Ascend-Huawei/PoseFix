062e05bd05dafb22434dab5aa23385e40a8cb3a5
Limin Zhu 84191691 <limin.zhu@huawei.com>
2021-08-18 14:43:50 -0700

    add default batch_size

--- main/train.py
+++ main/train.py
@@ -12,8 +12,8 @@ from tfflat.utils import mem_info
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=str, dest='gpu_ids')
-    parser.add_argument('--train_epochs', type= int, dest='train_epochs')
-    parser.add_argument('--batch_size', type= int, dest='batch_size')
+    parser.add_argument('--train_epochs', type= int, dest='train_epochs',default=1)
+    parser.add_argument('--batch_size', type= int, dest='batch_size',default=16)
     parser.add_argument('--continue', dest='continue_train', action='store_true')
     args = parser.parse_args()
 
